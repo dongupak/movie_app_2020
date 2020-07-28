12d5f1c4088b56a88d1c115d7003700560190750
DongGyu Park <dongupak@gmail.com>
2020-07-28 18:28:42 +0900

    movie

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
+function Potato() {
+  return <h1>I love Potato</h1>
+}
+
+function Apple() {
+  return <h1>I love Apple</h1>
+}
+
 function App() {
-  return (
-    <div className="App">;
+  return <div> 
+    <h1> Hello </h1>
+    <Potato />
+    </div>
 }
 
 export default App;
---
+++ src/Apple.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Apple(){
+    return <h3>I love apple</h3>;
+}
+
+export default Apple;
